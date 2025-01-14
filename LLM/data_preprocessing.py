f7de935018eae3289e5fcbe67a3bac17ef025afb
golchinpg <golchinpg@gmail.com>
2025-01-14 18:16:12 +0100

    adding load_text_data and tokenize_text functions to data preprocessing

--- LLM/data_preprocessing.py
+++ LLM/data_preprocessing.py
@@ -1,10 +1,21 @@
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import LabelEncoder, StandardScaler
+import tiktoken
 
-def load_data(file_path):
+def load_tabular_data(file_path):
     data = pd.read_csv(file_path)
     return data
+
+def load_text_data(file_path):
+    with open(file_path, 'r', encoding='utf-8') as f:
+        raw_text = f.read()
+    return raw_text
+
+def tokenize_text(text):
+    tokenizer = tiktoken.get_encoding("gpt2")
+    tokenized_text = tokenizer.encode(text, allowed_special={"<|endoftext|>"})
+    return tokenized_text
 
 def preprocess_data(data,target_name):
     X = data.drop(target_name, axis=1)
