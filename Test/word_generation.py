29baa849de8f999fd0f5f410f322a87d464d3a45
golchinpg <golchinpg@gmail.com>
2025-01-20 18:05:22 +0300

    adding softmax sampling to the word generation

--- Test/word_generation.py
+++ Test/word_generation.py
@@ -20,7 +20,8 @@ GPT_CONFIG_124M = {
     }
 
 # Load and preprocess data
-def generate_text_simple(model, idx, max_new_tokens, context_size):
+def generate_text_simple(model, idx, max_new_tokens, context_size,
+                        temprature = 0.0, top_k = None, eos_id = None):
     if idx.dim() == 1:
         idx = idx.unsqueeze(0)
     # idx is (batch, n_tokens) array of indices in the current context
@@ -40,11 +41,28 @@ def generate_text_simple(model, idx, max_new_tokens, context_size):
         # (batch, n_tokens, vocab_size) becomes (batch, vocab_size)
         logits = logits[:, -1, :]  
 
-        # Apply softmax to get probabilities
-        probas = torch.softmax(logits, dim=-1)  # (batch, vocab_size)
+        if top_k is not None:
+            # Apply top-k sampling
+            top_logits, _ = torch.topk(logits, top_k)
+            min_value = top_logits[:, -1]
+            logits = torch.where(logits<min_value, torch.tensor(float('-inf').to(logits.device)), logits)
 
-        # Get the idx of the vocab entry with the highest probability value
-        idx_next = torch.argmax(probas, dim=-1, keepdim=True)  # (batch, 1)
+        if temprature != 0.0:
+            # Apply temperature scaling
+            logits = logits / temprature
+
+            # Apply softmax to get probabilities
+            probas = torch.softmax(logits, dim=-1)  # (batch, vocab_size)
+
+            #sample from the distribution
+            idx_next = torch.multinomial(probas, num_samples=1)  # (batch, 1)
+        else:
+            # Get the idx of the vocab entry with the highest probability value
+            idx_next = torch.argmax(logits, dim=-1, keepdim=True)
+
+        if idx_next.item() == eos_id:
+            break
+        
 
         # Append sampled index to the running sequence
         idx = torch.cat((idx, idx_next), dim=1)  # (batch, n_tokens+1)
